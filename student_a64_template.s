c60f68bbe43219e6be044fb1480895b7a8e16f8c
al8703 <annliu2003@gmail.com>
2022-10-19 22:54:51 -0500

    update

--- student_a64_template.s
+++ student_a64_template.s
@@ -89,8 +89,8 @@ str_len:
     stur x9, [x0]
     movz x0, #0
     while_1:
-    ldurb x10,[x9], #1
-    adds x10, x10, #0
+    ldurb w10,[x9], #1
+    adds w10, w10, #0
     b.eq .L1
     adds x0, x0, #1
     adds x9, x9, #1
